8e21dd4553080f6d131a12ec32d79a99437cd994
AppVeyr <ci@appveyor.com>
2023-07-09 14:58:19 +0300

    del

--- src/components/ImageList/Image/Image.tsx
+++
@@ -1,9 +0,0 @@
-import './Image.scss';
-
-export default function Image({image}: {image: string}): JSX.Element {
-  return (
-    <li className="img-list__item img-item">
-      <img src={image} alt="" className="img-item__img" />
-    </li>
-  )
-}
